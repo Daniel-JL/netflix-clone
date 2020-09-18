1fb92eaa2338ca7132beedab75427f9d5033fdfd
Daniel-JL <danieljameslinehan@hotmail.com>
2020-09-18 16:18:30 +0200

    Got infinite scroll working. Scroll now loads components when it gets to bottom of page. Needs to be optimised but the functionality is there.

--- src/components/views/home.jsx
+++ src/components/views/home.jsx
@@ -1,5 +1,6 @@
-import React from 'react';
-import { 
+/* eslint-disable import/prefer-default-export */
+import React, { useState, useRef, useEffect } from 'react';
+import {
   NavLink,
 } from 'react-router-dom';
 import styled from 'styled-components';
@@ -7,8 +8,59 @@ import { MotionBackground } from '../motion-background';
 import { LocoRow } from '../loco-row';
 import { LolomoBigRow } from '../lolomo-big-row';
 
+const LocoRowGroup = () => (
+  <div>
+    <LocoRow />
+    <LocoRow />
+    <LocoRow />
+    <LocoRow />
+    <LocoRow />
+  </div>
+);
+
 export const Home = () => {
-  return(
+  let scrollGroupActive = useRef(0);
+  const [test, setTest] = useState(false);
+  const [element, setElement] = useState(null);
+
+  const prevY = useRef(0);
+  const observer = useRef(
+    new IntersectionObserver(
+      (entries) => {
+        const firstEntry = entries[0];
+        const { y } = firstEntry.boundingClientRect;
+
+        if (prevY.current > y) {
+          mountMoreComponents();
+        }
+
+        prevY.current = y;
+      },
+      { threshold: 0.5 },
+    ),
+  );
+
+  const mountMoreComponents = () => {
+    scrollGroupActive.current++;
+    setTest(test => !test);
+  };
+
+  useEffect(() => {
+    const currentElement = element;
+    const currentObserver = observer.current;
+
+    if (currentElement) {
+      currentObserver.observe(currentElement);
+    }
+
+    return () => {
+      if (currentElement) {
+        currentObserver.unobserve(currentElement);
+      }
+    };
+  }, [element]);
+
+  return (
     <div>
       <MotionBackground />
       <LocoRow />
@@ -16,7 +68,14 @@ export const Home = () => {
       <LocoRow />
       <LocoRow />
       <LolomoBigRow />
+      <LocoRow />
+      <LocoRow />
+      {(scrollGroupActive.current > 0) && <LocoRowGroup />}
+      {scrollGroupActive.current > 1 && <LocoRowGroup />}
+      {scrollGroupActive.current > 2 && <LocoRowGroup />}
+      {scrollGroupActive.current > 3 && <LocoRowGroup />}
+      {scrollGroupActive.current > 4 && <LocoRowGroup />}
+      <div ref={setElement} />
     </div>
-
-  )
-}
+  );
+};
